86be5b105279397fd5f2de5ba59c3bd319712053
tkhn0625 <athleta.0315@gmail.com>
2021-04-17 08:47:32 +0900

    created crud funcion

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import recipeRoutes from './routes/recipes';
+import { json } from 'body-parser';
 
 const app = express();
+
+// リクエストボディをJson型で受け取れるようにする。
+app.use(json());
+
+// /recipes へのリクエストは全てrecipeRoutesで処理を行う。
+app.use('/recipes', recipeRoutes);
+
+//　エラー時の処理
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
